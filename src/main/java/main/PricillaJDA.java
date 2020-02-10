5e18070e310be7a8912ec61e322bd8e82d80a5ff
badstagram <52403515+badstagram@users.noreply.github.com>
2020-02-10 10:33:20 +0000

    Update PricillaJDA.java

--- src/main/java/main/PricillaJDA.java
+++ src/main/java/main/PricillaJDA.java
@@ -9,7 +9,7 @@ import javax.security.auth.login.LoginException;
 public class PricillaJDA {
     public static JDA jda;
 //    Token from https://discordapp.com/developers/applications/
-    private static final String TOKEN = "NjY5OTI0MDc5MzM1Mzc0ODk4.XjVipg.zi56ICrq097kLjRadM0NAi4NN34";
+    private static final String TOKEN = "<Bot Token Removed>4";
 
     public static void main(String[] args) throws LoginException {
         jda = new JDABuilder(AccountType.BOT).setToken(TOKEN).build();
